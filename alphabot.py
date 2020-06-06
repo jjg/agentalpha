2d0a0073cd8e486eca678da215809fb587829c05
Jason J. Gullickson <mr@jasongullickson.com>
2020-06-06 13:53:53 -0500

    Its not efficient or well-written, but basic markov replys are functional.

--- alphabot.py
+++ alphabot.py
@@ -13,18 +13,21 @@ class AlphaBot(PineappleBot):
 
   @reply
   def respond(self, mention, user):
+
     # Extract the text of the mention
     soup = BeautifulSoup(mention["content"])
     mention_string = soup.get_text()
     print(mention_string)
+
+    # Load the corpus
+    corpus = open("./uni.txt","r")
+
     # generate markov model
-    markov_model = markovify.Text(mention_string)
+    markov_model = markovify.Text(corpus.read() + " " + mention_string)
+
     # Reply with a generated sentence
-    #print(mention)
-    #print("---")
-    #print(user)
-    #self.mastodon.toot(f"@{user['acct']}, {mention['content']}")
     self.mastodon.toot(f"@{user['acct']}, {markov_model.make_sentence()}")
+    corpus.close()
 
   def stop(self):
     self.mastodon.toot("Offline")
