157c50b5fd4fc9af94957adacb337de69db9be39
Jason J. Gullickson <mr@jasongullickson.com>
2020-06-06 14:49:31 -0500

    Switched corpus, added dynamic hourly toots, additional logging.

--- alphabot.py
+++ alphabot.py
@@ -7,26 +7,38 @@ class AlphaBot(PineappleBot):
   def start(self):
     self.mastodon.toot("Online")
 
-  @hourly(minute=5)
+  @hourly(minute=40)
   def say_something(self):
-    self.mastodon.toot("It's 5 after somewhere...")
+    # Load the corpus
+    corpus = open("./a.txt","r")
+
+    # generate markov model
+    markov_model = markovify.Text(corpus.read())
+    self.mastodon.toot(markov_model.make_sentence())
+
+    corpus.close()
 
   @reply
   def respond(self, mention, user):
 
     # Extract the text of the mention
-    soup = BeautifulSoup(mention["content"])
+    soup = BeautifulSoup(mention["content"], features="html.parser")
     mention_string = soup.get_text()
     print(mention_string)
 
     # Load the corpus
-    corpus = open("./uni.txt","r")
+    corpus = open("./a.txt","r")
 
     # generate markov model
     markov_model = markovify.Text(corpus.read() + " " + mention_string)
+    
+    # Generate a reply
+    reply_string = markov_model.make_sentence()
+    print(reply_string)
 
     # Reply with a generated sentence
-    self.mastodon.toot(f"@{user['acct']}, {markov_model.make_sentence()}")
+    self.mastodon.toot(f"@{user['acct']}, {reply_string}")
+
     corpus.close()
 
   def stop(self):
