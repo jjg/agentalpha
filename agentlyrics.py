86421b5863596fd4caee2080608ef38e7dfb5e1b
Jason J. Gullickson <mr@jasongullickson.com>
2020-06-26 10:05:53 -0500

    Experimenting with a new agent.

---
+++ agentlyrics.py
@@ -0,0 +1,22 @@
+from ananas import PineappleBot, hourly, reply
+
+class AgentLyrics(PineappleBot):
+
+    def get_lyric(self):
+        #TODO: Read a random line from the lyric file
+        lyric = "They're fucking with me, subliminally!"
+        return lyric
+
+    def start(self):
+        self.mastodon.toot(self.get_lyric())
+
+    @hourly(minute=10)
+    def hourly_lyric(self):
+        self.mastodon.toot(self.get_lyric())
+
+    @reply
+    def reply_lyric(self, mention, user):
+        self.mastodon.toot(f"@{user['acct']}, {self.get_lyric()}")
+
+    def stop(self):
+        self.mastodon.toot(self.get_lyric())
