eb2f87ca58a9007f9390abe03a0c744410cf7adf
Jason J. Gullickson <mr@jasongullickson.com>
2020-06-27 09:51:56 -0500

    Initial pass at creating a ROM construct of myself.

--- agents/romconstruct.py
+++ agents/romconstruct.py
@@ -3,11 +3,7 @@ import markovify
 
 class ROMConstruct(PineappleBot):
 
-  def start(self):
-    self.mastodon.toot(f"{self.config.name} coming online")
-
-  @reply
-  def respond(self, mention, user):
+  def model_text(self):
 
     # Load the corpus
     with open(self.config.rom_file,"r") as corpus:
@@ -15,11 +11,21 @@ class ROMConstruct(PineappleBot):
         # generate markov model
         markov_model = markovify.Text(corpus.read(), state_size=3)
     
-        # Generate a reply
-        reply_string = markov_model.make_sentence()
+        return markov_model.make_sentence()
+
+  def start(self):
+    self.mastodon.toot(f"{self.config.name} coming online")
+
+    # Say a little somthing to test things out
+    self.mastodon.toot(self.model_text())
+
+  @reply
+  def respond(self, mention, user):
 
         # Reply with a generated sentence
-        self.mastodon.toot(f"@{user['acct']}, {reply_string}")
+        # TODO: Figure out how to call the API so that this is
+        # a properly-threaded reply
+        self.mastodon.toot(f"@{user['acct']}, {self.model_text()}")
 
   def stop(self):
     self.mastodon.toot(f"{self.config.name} going offline")
